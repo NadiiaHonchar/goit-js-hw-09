3e8415e86159c4dda313f5e2f1b757fda95d2eb8
NadiiaHonchar <17nmg82@gmail.com>
2021-11-18 00:08:28 +0200

    add imports

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,3 +1,6 @@
+import flatpickr from "flatpickr";
+import "flatpickr/dist/flatpickr.min.css";
+
 function createPromise(position, delay) {
   const shouldResolve = Math.random() > 0.3;
   if (shouldResolve) {
