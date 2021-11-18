72290a8420847b81933d97b24560a184068437f8
NadiiaHonchar <17nmg82@gmail.com>
2021-11-18 20:07:14 +0200

    add collor-swircher

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,22 @@
+function getRandomHexColor() {
+return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}  
 
+const getStart = document.querySelector('button[data-start]');
+const getStop = document.querySelector('button[data-stop]');  
+
+getStop.disabled = true;
+
+getStart.addEventListener("click", () => {
+timerId = setInterval(() => {
+    document.body.style.backgroundColor = getRandomHexColor(); 
+    getStart.disabled = true;
+    getStop.removeAttribute('disabled');       
+}, 1000);
+});
+
+getStop.addEventListener("click", () => {
+clearInterval(timerId);
+getStart.removeAttribute('disabled');
+getStop.disabled = true;    
+});
