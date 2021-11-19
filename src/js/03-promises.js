e2e095ea0c964d4cb265ff44189974f8d05d2714
NadiiaHonchar <17nmg82@gmail.com>
2021-11-19 23:20:49 +0200

    add promises

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,11 +1,39 @@
-import flatpickr from "flatpickr";
-import "flatpickr/dist/flatpickr.min.css";
+const form = document.querySelector('.form');
+  
+form.addEventListener('submit', onFormSubmit);
 
 function createPromise(position, delay) {
-  const shouldResolve = Math.random() > 0.3;
-  if (shouldResolve) {
-    // Fulfill
-  } else {
-    // Reject
+  return new Promise((resolve, reject) =>{
+    const shouldResolve = Math.random() > 0.3;
+
+    setTimeout(() =>{
+    if (shouldResolve) {
+      resolve(`✅ Fulfilled promise ${position} in ${delay}ms`)
+    } 
+      reject(`❌ Rejected promise ${position} in ${delay}ms`)    
+  }, delay);
+  });
+  
+};
+
+function onFormSubmit(event) {
+  event.preventDefault();
+  if (!event.currentTarget.elements.delay.value || !event.currentTarget.elements.step.value|| !event.currentTarget.elements.amount.value) {
+    alert('Please, enter data');
   }
+  const formData = new FormData(event.currentTarget);
+  let delay = Number(event.currentTarget.elements.delay.value);
+  for (let i =0; i < event.currentTarget.elements.amount.value; i+=1){
+    let position = i+1;
+    createPromise(position, delay)
+  .then((result) => {    
+    console.log(result);
+  })
+  .catch((result) => {    
+    console.log(result);
+  });
+
+    delay = delay + Number(event.currentTarget.elements.step.value);    
+  }
+  form.reset();
 }
